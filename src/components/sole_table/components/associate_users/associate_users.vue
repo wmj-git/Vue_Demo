2d4b355387a1b5b4e7359261522f5e93544b391d
gfl <1130357243@qq.com>
2019-07-01 10:53:38 +0800

    关联用户功能完成

---
+++ src/components/sole_table/components/associate_users/associate_users.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="associate_users">
+      <el-form>
+          <el-form-item>
+             <el-transfer v-model="value" :data="data"
+              :titles="['全部角色', '选中角色']">
+             </el-transfer>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitAssociate">确认修改</el-button>
+            <el-button @click="cancel">取消</el-button>
+          </el-form-item>
+      </el-form>
+    </div>
+</template>
+
+<script>
+  import {roleList,setRoles} from "@/api/table_operate"
+  export default {
+    data() {
+      const generateData = _ => {
+        const data = [];
+        roleList().then(res=>{           //得到角色列表
+          res.data.list.forEach((_val)=>{
+            data.push({key:_val.id,label:_val.roleCname})
+          });
+
+        });
+        return data
+      };
+      return {
+        data: generateData(),      //供选择的角色列表赋值给data
+        value: [],
+        userId:"" ,    //要修改的用户的id
+      };
+    },
+    methods:{
+      recieveRoles(userId){
+        this.userId=userId;
+      },
+      submitAssociate(){
+        console.log(this.value);
+        setRoles({"roleIds":this.value,"userId":this.userId}).then(res=>{
+          console.log(res);
+          if(res.statusCode==200){
+            this.$message({
+              message: '关联用户成功',
+              type: 'success'
+            });
+            this.$emit("closeCommonDialog")
+          }
+        });
+      },
+      cancel(){
+        this.$emit("closeCommonDialog")
+      }
+    }
+  };
+</script>
+<style scoped lang="scss">
+  @import "_associate_users";
+</style>
---
+++ src/components/sole_table/components/role_assinments/role_assinments.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="role_assinments">
+      <el-form  label-width="100px">
+        <el-form-item label="角色">
+          <el-select v-model="roles" multiple placeholder="请选择">
+            <el-option
+              v-for="item in roleOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitRoles">确认修改</el-button>
+          <el-button @click="cancel">取消</el-button>
+        </el-form-item>
+      </el-form>
+
+    </div>
+</template>
+
+<script>
+  import {roleList,setRoles} from "@/api/table_operate"
+    export default {
+        data(){
+            return{
+              roles:"" ,    //选择的角色
+              userId:"",     //要修改的用户的id
+              roleOptions: [],// 角色管理的选项
+            }
+        },
+      beforeCreate(){
+          let roleArr=[];
+          roleList().then(res=>{           //得到角色列表
+            res.data.list.forEach((_val)=>{
+              roleArr.push({"value":_val.id,"label":_val.roleCname})
+            });
+            this.roleOptions=roleArr;
+            console.log( this.roleOptions)
+          })
+
+      },
+        methods:{
+          recieveRoles(userId){
+            this.userId=userId;
+          },
+          submitRoles(){               //提交修改好的角色
+            console.log(this.roles);
+            setRoles({"roleIds":this.roles,"userId":this.userId}).then(res=>{
+              console.log(res);
+              if(res.statusCode==200){
+                this.$message({
+                  message: '角色分配成功',
+                  type: 'success'
+                });
+                this.$emit("closeCommonDialog")
+              }
+            });
+
+          },
+          cancel(){
+            this.$emit("closeCommonDialog")
+          }
+        }
+    };
+</script>
