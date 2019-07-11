75d10f323453dd25b0ccc72532dfaebc6ec055c2
gfl <1130357243@qq.com>
2019-07-11 09:23:01 +0800

    彻底解决冲突

--- src/views/main/components/em_menu/em_menu.vue
+++ src/views/main/components/em_menu/em_menu.vue
@@ -34,21 +34,13 @@
         console.log(key);
         let _key = null;
         this.group.forEach(function (_obj) {
-<<<<<<< HEAD
-            if(key===_obj.id){
-=======
             if(key===_obj.system_id){
->>>>>>> 6f2e57197f53efb31f52954bd0a3c1ff872f8cd9
               _key=_obj.control_id;
             }
         });
         this.$store.commit('win/win_open', {
           win_obj: {
-<<<<<<< HEAD
-            id: _key
-=======
             system_id: _key
->>>>>>> 6f2e57197f53efb31f52954bd0a3c1ff872f8cd9
           }
         });
       }
@@ -61,13 +53,8 @@
       this.bus.$on(this.id, function (obj) {
         console.log(obj);
         this.group = obj.list;
-<<<<<<< HEAD
-        $("#" + this.id).window("setTitle", obj.title);
-        $("#" + this.id).window("resize", {
-=======
         $("#" + obj.id).window("setTitle", obj.title);
         $("#" + obj.id).window("resize", {
->>>>>>> 6f2e57197f53efb31f52954bd0a3c1ff872f8cd9
           width: obj.width
         });
 
