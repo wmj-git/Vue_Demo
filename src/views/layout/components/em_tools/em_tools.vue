45af8d84ff68dc23dd18305b5ab7308bb228eb0b
linda <linda@sina.com>
2019-07-31 14:07:36 +0800

    样式优化

--- src/views/layout/components/em_tools/em_tools.vue
+++ src/views/layout/components/em_tools/em_tools.vue
@@ -1,109 +1,110 @@
 <template>
-    <div class="em_tool">
-        <win :id="win.id" :data="win">
-            <div class="em-nav">
-                <input type="checkbox" class="em-nav-cb" id="menu-cb">
-                <div class="em-nav-content">
-                    <ul class="em-nav-items" id="subnav">
-                        <li v-for="(item,key) in tool" class="em-nav-item">
-                            <!-- <span class="nav__item-text" prefix-icon="el-icon-users">{{item}}</span>-->
-                            <el-tooltip class="item" placement="top-start">
-                                <div slot="content">{{item.value}}</div>
-                                <el-button :ref="item.id" :icon="item.icon" class="em-nav-item-text"
-                                           @click="fn(item,key)"></el-button>
-                            </el-tooltip>
-                        </li>
-                    </ul>
-                </div>
-                <label class="em-nav-btn" for="menu-cb"></label>
-            </div>
-        </win>
-    </div>
+  <div class="em_tool">
+    <win :id="win.id" :data="win">
+      <div class="em-nav">
+        <input type="checkbox" class="em-nav-cb" id="menu-cb">
+        <div class="em-nav-content">
+          <ul class="em-nav-items" id="subnav">
+            <li v-for="(item,key) in tool" class="em-nav-item">
+              <!-- <span class="nav__item-text" prefix-icon="el-icon-users">{{item}}</span>-->
+              <el-tooltip class="item" placement="top-start">
+                <div slot="content">{{item.value}}</div>
+                <el-button :ref="item.id" :icon="item.icon" class="em-nav-item-text"
+                           @click="fn(item,key)"></el-button>
+              </el-tooltip>
+            </li>
+          </ul>
+        </div>
+        <label class="em-nav-btn" for="menu-cb"></label>
+      </div>
+    </win>
+  </div>
 </template>
 
 <script>
-    import win from "@/components/win/win"
+  import win from "@/components/win/win"
 
-    export default {
-        name: "em_bottom",
-        components: {
-            win
+  export default {
+    name: "em_bottom",
+    components: {
+      win
+    },
+    data() {
+      return {
+        win: {
+          id: "em_tool",
+          title: "",
+          top: "",
+          left: "calc(100% - 450px)",
+          right: 20,
+          show: true,
+          width: 450,
+          resizable: false,
+          class: "em-tool-window"
         },
-        data() {
-            return {
-                win: {
-                    id: "em_tool",
-                    title: "",
-                    top: "",
-                    left: "calc(100% - 450px)",
-                    right: 20,
-                    show: true,
-                    width: 450,
-                    resizable: false,
-                    class: "em-tool-window"
-                },
-                input: ''
-            }
-        },
-        created() {
-        },
-        computed: {
-            tool: function () {
-                return this.$store.getters["scene/toolbar"];
-            }
-        },
-        mounted() {
-            var subnav = document.getElementById('subnav'),
-                aLi = document.querySelectorAll('#subnav li'),
-                w = parseFloat(subnav.offsetWidth / aLi.length);//通过ul的宽度除以li的个数来计算每个li的宽度
-            console.log("个数：" + aLi.length);
-            for (var i = 0; i < aLi.length; i++) {
-                aLi[i].style.width = w + 'px';
-            }
-        },
-        methods: {
-            fn(item, key) {
-                console.log(item);
-                console.log(key);
-                
-                switch (item.control_type) {
-                    case "tool":
-                        this[item.fn](item);
-                        break;
-                    case "scene":
-                        if(!(item.control_id)){
-                            item.control_id=this.$store.getters["scene/type"];//获取地图对象
-                        }
-                        this.bus.$emit(item.control_id, item);
-                        if (item.trigger !== "none") {
-                            let _item = this.tool[key];
-                            this.tool[key].trigger = !(_item.trigger);
-                            console.log(this.tool[key].trigger);
-                        }
-                        break;
-                }
-
-            },
-            handleData(_obj) {
-                let _show = _obj.trigger;
-                let _controlId= _obj.control_id;
-                if (_show) {
-                    this.$store.commit('win/win_open', {
-                        win_obj: {
-                            id: _controlId
-                        }
-                    });
-                } else {
-                    this.$store.commit('win/win_close', {
-                        win_id: _controlId
-                    });
-                }
-            }
+        input: ''
+      }
+    },
+    mounted() {
+      $(function () {
+        var subnav = document.getElementById('subnav'),
+          aLi = document.querySelectorAll('#subnav li'),
+          w = parseFloat(subnav.offsetWidth / aLi.length);//通过ul的宽度除以li的个数来计算每个li的宽度
+        console.log("个数：" + aLi.length);
+        for (var i = 0; i < aLi.length; i++) {
+          aLi[i].style.width = w + 'px';
         }
+      });
+
+    },
+    computed: {
+      tool: function () {
+        return this.$store.getters["scene/toolbar"];
+      }
+    },
+    methods: {
+      fn(item, key) {
+        console.log(item);
+        console.log(key);
+
+        switch (item.control_type) {
+          case "tool":
+            this[item.fn](item);
+            break;
+          case "scene":
+            if (!(item.control_id)) {
+              item.control_id = this.$store.getters["scene/type"];//获取地图对象
+            }
+            this.bus.$emit(item.control_id, item);
+            if (item.trigger !== "none") {
+              let _item = this.tool[key];
+              this.tool[key].trigger = !(_item.trigger);
+              console.log(this.tool[key].trigger);
+            }
+            break;
+        }
+
+      },
+      handleData(_obj) {
+        let _show = _obj.trigger;
+        let _controlId = _obj.control_id;
+        if (_show) {
+          this.$store.commit('win/win_open', {
+            win_obj: {
+              id: _controlId
+            }
+          });
+        } else {
+          this.$store.commit('win/win_close', {
+            win_id: _controlId
+          });
+        }
+      }
     }
+  }
 </script>
 
 <style lang="scss" scoped>
-    @import "em_tools";
+  @import "em_tools";
 </style>
 
