4b8730e96ec5db30f667cddddf5d5ce412f29555
yfb-lj <838108200@qq.com>
2019-09-03 09:04:43 +0800

    0902

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,13 +5,13 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"http://zhlh.cqemme.com:6733/api/v1"',
-  BASE_API: '"http://amkj.cqemme.com:1800/api/v1"',
-  // BASE_API: '"http://192.168.20.18:1800/api/v1"',
+  // BASE_API: '"http://amkj.cqemme.com:1800/api/v1"',
+  BASE_API: '"http://192.168.20.18:1800/api/v1"',
 
   IMG_API: '"http://zhlh.cqemme.com:6733"',
 
-  STATIC_URL: '"http://amkj.cqemme.com:1800"'//场景资源地址
-  // STATIC_URL: '"http://192.168.20.18:1800"'//场景资源地址
+  // STATIC_URL: '"http://amkj.cqemme.com:1800"'//场景资源地址
+  STATIC_URL: '"http://192.168.20.18:1800"'//场景资源地址
 })
 
 // process.env.STATIC_URL
