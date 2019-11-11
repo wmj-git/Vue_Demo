d1dadc9a28268c3ecdf34cbe3dad6f2f542ce98f
yfb-lj <838108200@qq.com>
2019-11-11 10:59:12 +0800

    1111

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -109,7 +109,7 @@
           localStorage.setItem('password','');
         }
         this.init();
-        this.$router.push("/main/map");
+        this.$router.push("/home/map");
       }
 
     }
